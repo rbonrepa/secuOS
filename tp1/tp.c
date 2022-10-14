600d16dd7b5592972eab6ce0d92cbdbe5abbf5d8
HeineKayn <thomashoger@hotmail.fr>
2022-10-14 13:15:04 +0200

    notes

--- tp1/tp.c
+++ tp1/tp.c
@@ -1,9 +1,12 @@
 /* GPLv2 (c) Airbus */
 #include <debug.h>
 #include <info.h>
+#include <segmem.h>
+
 
 extern info_t *info;
 
-void tp()
-{
+void tp(){
+    gdt_reg_t * gdt;
+    get_gdtr(gdt);
 }
--- utils/config.mk
+++ utils/config.mk
@@ -48,8 +48,8 @@ LDSCRIPT   := ../utils/linker.lds
 TARGET     := kernel.elf
 
 # Qemu options
-QEMU := $(shell which qemu-system-i386)
-#QEMU := $(shell which kvm)
+#QEMU := $(shell which qemu-system-i386)
+QEMU := $(shell which kvm)
 QFDA := -drive media=disk,format=raw,if=floppy,file=../utils/grub.floppy
 QHDD := -drive media=disk,format=raw,if=ide,index=0,file=fat:rw:.
 QSRL := -serial mon:stdio
