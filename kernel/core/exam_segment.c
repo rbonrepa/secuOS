f059c8cc1d467ddf90770235cd2c3769a62f0489
HeineKayn <thomashoger@hotmail.fr>
2022-11-19 17:21:53 +0100

    début segmentation

--- kernel/core/exam_segment.c
+++ kernel/core/exam_segment.c
@@ -1,1 +1,103 @@
 #include <exam_segment.h>
+#include <info.h>
+#include <debug.h>
+
+extern info_t *info;
+
+void display_gdt(gdt_reg_t *gdtr)
+{
+  int i = 0;
+  seg_desc_t *begin;
+  begin = (seg_desc_t *)gdtr->desc;
+  while (begin <= (seg_desc_t *)gdtr->desc + gdtr->limit / sizeof(seg_desc_t))
+  {
+    uint32_t addr = (begin->base_1) | (begin->base_2 << 16) | (begin->base_3 << 24);
+    uint32_t lim = (begin->limit_1) | (begin->limit_2 << 16);
+    if (begin->g)
+    {
+      lim += 1;
+      lim *= 4096;
+      lim -= 1;
+    }
+    if (begin->p)
+    {
+      printf(
+          "Segment %d : Adresse : [0x%x - 0x%x] : Type de descripteur: %d\n\n",
+          i, addr, lim, begin->type);
+    }
+    begin++;
+    i++;
+  }
+}
+
+seg_desc_t create_dsc_flat(uint64_t type, uint64_t dpl)
+{
+  seg_desc_t seg;
+
+  seg.limit_1 = 0xffff;
+  seg.limit_2 = 0xf;
+
+  seg.type = type; // segment type
+  seg.dpl = dpl;   // descriptor privilege level (ring)
+
+  seg.d = 1; // default length, depend on seg type
+  seg.g = 1; // granularity
+  seg.s = 1; // descriptor type
+  seg.p = 1; // segment present flag
+
+  return seg;
+}
+
+seg_desc_t create_dsc_tss(uint64_t limit, uint64_t base)
+{
+  seg_desc_t seg;
+  
+  seg.limit_1 = limit;
+  seg.limit_2 = (limit >> 2 * 8);
+
+  seg.base_1  = base;
+  seg.base_2  = (base >> 2 * 8);
+  seg.base_3  = (base >> 3 * 8);
+
+  seg.type   = SEG_DESC_SYS_TSS_AVL_32;         
+  seg.p      = 1; 
+
+  return seg;
+}
+
+void init_gdt()
+{
+  gdt_reg_t gdtr;
+
+  // Création de notre propre table gdt
+  seg_desc_t * GDT = (seg_desc_t *)0x0; // -------- A CHANGER QUAND ON AURA DECIDE DES @
+
+  // Segment vide rempli de 0, indice 0
+  for (uint32_t i = 0; i < sizeof(seg_desc_t); i++)
+  {
+    *((uint8_t *)GDT + i) = 0;
+  }
+
+  // Kernel
+  GDT[1] = create_dsc_flat(SEG_DESC_CODE_XR, 1);
+  GDT[2] = create_dsc_flat(SEG_DESC_DATA_RW, 1);
+
+  // User
+  GDT[3] = create_dsc_flat(SEG_DESC_CODE_XR, 3);
+  GDT[4] = create_dsc_flat(SEG_DESC_DATA_RW, 3);
+
+  // TSS (pas sûr de moi)
+  GDT[5] = create_dsc_tss(0xfffff, 0x0); // -------- SANS DOUTE A CHANGER
+
+  // Validation
+  gdtr.desc  = GDT;
+  gdtr.limit = 6 * sizeof(seg_desc_t) - 1; // ------ CHANGER PEUT ETRE PLUS TARD 
+  set_gdtr(gdtr);
+
+  set_cs(c0_sel); // -------- SANS DOUTE A CHANGER
+  set_ss(d0_sel);
+  set_ds(d0_sel);
+  set_es(d0_sel);
+  set_fs(d0_sel);
+  set_gs(d0_sel);
+}
--- kernel/include/exam_segment.h
+++ kernel/include/exam_segment.h
@@ -1,4 +1,18 @@
 #ifndef EXAM_SEGMENT
 #define EXAM_SEGMENT
 
+#include <segmem.h>
+#include <string.h>
+
+#define c0_sel  gdt_krn_seg_sel(1)
+#define d0_sel  gdt_krn_seg_sel(2)
+#define c3_sel  gdt_usr_seg_sel(3)
+#define d3_sel  gdt_usr_seg_sel(4)
+#define ts_sel  gdt_krn_seg_sel(5)
+
+void display_gdt(gdt_reg_t *gdtr);
+seg_desc_t create_dsc_flat(uint64_t type, uint64_t dpl);
+seg_desc_t create_dsc_tss(uint64_t limit, uint64_t base);
+void init_gdt();
+
 #endif
