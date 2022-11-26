769b5cb1729c499df777cb15d7b6ea235d0b4422
HeineKayn <thomashoger@hotmail.fr>
2022-11-26 18:03:31 +0100

    task init

--- tp_exam/tp.c
+++ tp_exam/tp.c
@@ -9,25 +9,27 @@ extern info_t *info;
 extern uint32_t __kernel_start__;
 extern uint32_t __kernel_end__;
 
-void tp()
-{
+void show_info(){
+    mbi_t *mbi;
+    multiboot_memory_map_t *start;
+    multiboot_memory_map_t *end;
+
+    mbi   = info->mbi;
+    start = (multiboot_memory_map_t*) mbi->mmap_addr;
+    end   = (multiboot_memory_map_t*)(mbi->mmap_addr + mbi->mmap_length);
+
+    while(start < end) {
+        debug("mmap 0x%llx - 0x%llx (%d)\n",
+                start->addr, start->addr+start->len, start->type);
+        start++;
+    }
 
     debug("kernel mem [0x%x - 0x%x]\n", (uint32_t)&__kernel_start__, (uint32_t)&__kernel_end__);
-    debug("MBI flags 0x%x\n", info->mbi->flags);
+    debug("MBI flags 0x%x\n", mbi->flags);
+}
 
-    // mbi_t *mbi;
-    // multiboot_memory_map_t *start;
-    // multiboot_memory_map_t *end;
-
-    // mbi   = info->mbi;
-    // start = (multiboot_memory_map_t*) mbi->mmap_addr;
-    // end   = (multiboot_memory_map_t*)(mbi->mmap_addr + mbi->mmap_length);
-
-    // while(start < end) {
-    //     debug("mmap 0x%llx - 0x%llx (%d)\n",
-    //             start->addr, start->addr+start->len, start->type);
-    //     start++;
-    // }
+void tp()
+{
     debug("---Initialisation GDT---\n");
     init_gdt();
     debug("---Display GDT---\n");
@@ -35,4 +37,6 @@ void tp()
     
     // debug("---Initialisation Pages---\n");
     // identity_init();
+
+    show_info();
 }
