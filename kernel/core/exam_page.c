6eeeed923c5660306f6d0e5ddecd5c9076752c1b
vmouton <vmouton@SecuOS.myguest.virtualbox.org>
2022-11-26 16:14:06 +0100

    Identity mapped fixed

--- kernel/core/exam_page.c
+++ kernel/core/exam_page.c
@@ -5,22 +5,26 @@ extern info_t *info;
 
 void display_pte(pte32_t *pte, uint32_t offset)
 {
-   for (uint32_t i=0; i<3;i++){
-      if (pte[i].addr > 0){
-         printf("-- (%d) : Virtuelle : 0x%x -> Physique : 0x%x\n", i, (i | (offset<<10)) << 12, pte[i].addr<<12);
+   for (uint32_t i = 0; i < 3; i++)
+   {
+      if (pte[i].addr > 0)
+      {
+         printf("-- (%d) : Virtuelle : 0x%x -> Physique : 0x%x\n", i, (i | (offset << 10)) << 12, pte[i].addr << 12);
       }
    }
 }
 
-void display_pgd(){
-   pde32_t *pgd = (pde32_t*)get_cr3();
+void display_pgd(pde32_t *pgd)
+{
    debug("---Display PGD---\n");
-   printf("Address PGD : %p\n", (void*)pgd);
+   printf("Address PGD : %p\n", (void *)pgd);
 
-   for (uint32_t i=0;i<PDE32_PER_PD;i++){
-      if (pgd[i].addr > 0){
-         printf("- Index : %d Address PTB : 0x%x\n",i, pgd[i].addr<<12);
-         display_pte((pte32_t *)(pgd[i].addr<<12), i);
+   for (uint32_t i = 0; i < PDE32_PER_PD; i++)
+   {
+      if (pgd[i].addr > 0)
+      {
+         printf("- Index : %d Address PTB : 0x%x\n", i, pgd[i].addr << 12);
+         display_pte((pte32_t *)(pgd[i].addr << 12), i);
       }
    }
 }
@@ -28,40 +32,40 @@ void display_pgd(){
 void enable_paging()
 {
    uint32_t cr0 = get_cr0();
-   set_cr0(cr0|CR0_PG);
+   set_cr0(cr0 | CR0_PG);
 }
 
-void init_pages(pde32_t* pgd, pte32_t* ptb, int lvl){
-   for (uint32_t i_pgd=0;i_pgd<PDE32_PER_PD;i_pgd++){
-
-      memset((void*)pgd, 0, PAGE_SIZE);
-      pg_set_entry(&pgd[i_pgd], lvl|PG_RW, page_nr(&ptb[i_pgd]));
-
-      for (uint32_t i_ptb=0;i_ptb<PTE32_PER_PT;i_ptb++){
-         memset((void*)&ptb[i_pgd], 0, PAGE_SIZE);
-         pg_set_entry(&ptb[i_pgd], lvl|PG_RW, i_ptb+1024);
-      } 
-  }
+void init_pages(pde32_t *pgd, pte32_t *ptb, int lvl)
+{
+   memset((void *)pgd, 0, PAGE_SIZE);
+   for (uint32_t i_pgd = 0; i_pgd < PDE32_PER_PD; i_pgd++)
+   {
+      pg_set_entry(&pgd[i_pgd], lvl | PG_RW, page_nr(&ptb[i_pgd * PTE32_PER_PT]));
+      for (uint32_t i_ptb = 0; i_ptb < PTE32_PER_PT; i_ptb++)
+      {
+         pg_set_entry(&ptb[i_ptb + i_pgd * PTE32_PER_PT], lvl | PG_RW, i_ptb + i_pgd * 1024);
+      }
+   }
 }
 
 void identity_init()
 {
-   enable_paging();
 
-   pde32_t *pgd_kernel  = (pde32_t*)0x600000; // taille PGD  = 0x400
-   pde32_t *pgd_user1   = (pde32_t*)0x601000;
-   pde32_t *pgd_user2   = (pde32_t*)0x602000;
+   pde32_t *pgd_kernel = (pde32_t *)0x600000; // taille PGD  = 0x400
+   pde32_t *pgd_user1 = (pde32_t *)0x601000;
+   pde32_t *pgd_user2 = (pde32_t *)0x602000;
 
-   pte32_t *ptbs_kernel = (pte32_t*)0x700000; // taille PTBS = 0x100000
-   pte32_t *ptbs_user1  = (pte32_t*)0x800000;
-   pte32_t *ptbs_user2  = (pte32_t*)0x900000;
+   pte32_t *ptbs_kernel = (pte32_t *)0xf00000; // taille PTBS = 0x100000
+   pte32_t *ptbs_user1 = (pte32_t *)0xb00000;
+   pte32_t *ptbs_user2 = (pte32_t *)0x900000;
 
-   init_pages(pgd_kernel, ptbs_kernel, PG_KRN); 
-   init_pages(pgd_user1,  ptbs_user1,  PG_USR);
-   init_pages(pgd_user2,  ptbs_user2,  PG_USR);
+   init_pages(pgd_kernel, ptbs_kernel, PG_KRN);
+   init_pages(pgd_user1, ptbs_user1, PG_USR);
+   init_pages(pgd_user2, ptbs_user2, PG_USR);
 
    set_cr3(pgd_kernel);
-   display_pgd();
+   enable_paging();
+   display_pgd(pgd_user2);
 
    // ----------
 
